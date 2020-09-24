4ef2f1489538a0e97be7c4265d50e7f735f55c2b
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-24 23:40:39 +0000

    second part

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 // The code below is temporary and can be commented out.
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -10,4 +12,16 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+// The code below is temporary and can be commented out.
+
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
 });
